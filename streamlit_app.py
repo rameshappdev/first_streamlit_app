e009f38bb313bf72f56ba1be25f1e03874849d81
rameshappdev <131317417+rameshappdev@users.noreply.github.com>
2023-04-19 21:37:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+
+streamlit.title('Breakfast Menu')
+
+streamlit.title('Oats and Milk')
+
+
+streamlit.title('Fresh fruits and Vegetables')
+
+streamlit.title('Almonds and nuts')
